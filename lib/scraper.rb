0052b462daae996a2ba9de0aaa96344d8909cb3f
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-13 20:39:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+#html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc
+#doc.css(".headline-26OIBN")
